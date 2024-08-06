0c412a4f7a9945bc741596e32d40e8d6b4cfc1e6
Gabriel Tomazine <gtomazine1@gmail.com>
2024-08-06 14:26:44 -0300

    Coloca os exercícios das Olimpiadas no repositório

---
+++ Aula 01/script.js
@@ -0,0 +1,18 @@
+//QUESTAO 4
+
+let t = 0
+
+function e(t) {
+    return (t ** 2) + 3*t + 50
+}
+
+//console.log(e(5))
+
+
+
+//QUESTAO 3
+function h(t) {
+    return  (-5*t**2) + 20*t
+}
+
+console.log(h(2))
