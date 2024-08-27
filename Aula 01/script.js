b0627a331a8c0c1047031daabe39cf131f3abedd
Gabriel <gtomazine1@gmail.com>
2024-08-27 14:34:32 -0300

    Arquivos Aula PHP

--- Aula 01/script.js
+++ Aula 01/script.js
@@ -1,3 +1,11 @@
+//QUESTAO 3
+function h(t) {
+    return  (-5*t**2) + 20*t
+}
+
+console.log("Altura Máxima:",h(2))
+
+
 //QUESTAO 4
 
 let t = 0
@@ -6,13 +14,6 @@ function e(t) {
     return (t ** 2) + 3*t + 50
 }
 
-//console.log(e(5))
+console.log("Número de Espectadores após 5 horas:",e(5))
 
 
-
-//QUESTAO 3
-function h(t) {
-    return  (-5*t**2) + 20*t
-}
-
-console.log(h(2))
