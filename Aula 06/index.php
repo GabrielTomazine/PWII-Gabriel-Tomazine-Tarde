0495a4a5cc6cd0aa9c1946e56692647d25712381
Gabriel <gtomazine1@gmail.com>
2024-10-29 14:38:10 -0300

    Aula 06

---
+++ Aula 06/index.php
@@ -0,0 +1,52 @@
+<?php
+/*
+
+//Example "While":
+$i = 1;
+while ($i < 6) {
+  echo $i;
+  $i++;
+}
+
+//Example "Do While":
+$i = 1;
+do {
+  echo $i;
+  $i++;
+} while ($i < 6);
+
+//Example "For":
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+  }
+
+//Example "Foreach":
+$colors = array("red","green","yelow","blue");
+
+foreach ($colors as $color) {
+echo "$color <br>";
+}
+
+//Example "Break":
+for ($x = 0; $x < 10; $x++) {
+    if ($x == 4) {
+      break;
+    }
+    echo "The number is: $x <br>";
+  }
+
+//Example "Continue":
+for ($x = 0; $x < 10; $x++) {
+    if ($x == 4) {
+      continue;
+    }
+    echo "The number is: $x <br>";
+  }
+*/
+
+$numeros = array("2","5","7","8","10","18","30","37","38","60");
+
+foreach ($numeros as $num){
+    
+}
+?>
